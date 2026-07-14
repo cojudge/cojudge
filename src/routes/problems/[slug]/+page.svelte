0e958e9850ad269c209d662f4b76984502d91734
wai.y <knyl2013@gmail.com>
2026-07-13 22:52:10 -0400

    fix: Hide share code button when firebase configs are absent

--- src/routes/problems/[slug]/+page.svelte
+++ src/routes/problems/[slug]/+page.svelte
@@ -871,15 +871,17 @@
                         </button>
                     </Tooltip>
                 {/if}
-                <Tooltip text={"Share Code"} pos={"bottom"}>
-                    <button class="icon-button" on:click={handleShare} title="Share Code">
-                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            <polyline points="16 6 12 2 8 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            <line x1="12" y1="2" x2="12" y2="15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                    </button>
-                </Tooltip>
+                {#if isFirebaseAvailable}
+                    <Tooltip text={"Share Code"} pos={"bottom"}>
+                        <button class="icon-button" on:click={handleShare} title="Share Code">
+                            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <polyline points="16 6 12 2 8 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <line x1="12" y1="2" x2="12" y2="15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                    </Tooltip>
+                    {/if}
                 <Tooltip text={"Reset Code"} pos={"bottom"}>
                     <button
                         class="icon-button"
