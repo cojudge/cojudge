54b135c65a8e16c763528eb4af654e52a5797fa7
wai.y <knyl2013@gmail.com>
2026-08-05 21:06:50 -0400

    misc: Remove configs from cojudge.cloud

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [
 		sveltekit()
-	],
-	server: {
-		allowedHosts: ['cojudge.cloud', 'cojudge.waiyip.life']
-	}
+	]
 });
